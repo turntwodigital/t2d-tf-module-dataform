b5e7355081fab847475debb3ae896cc056dcf976
Krisjan O. <krisjan@gmail.com>
2023-12-11 12:02:18 +0000

    Switch to default service account

--- service_accounts.tf
+++ service_accounts.tf
@@ -1,26 +1,26 @@
 # Set up service account for Dataform
-resource "google_service_account" "dataform" {
-    project      = var.project_id
-    account_id   = "${var.resource_prefix}-gsa-dataform"
-    display_name = "Service Account for Dataform (executing Dataform Workflows)"
-}
+#resource "google_service_account" "dataform" {
+#    project      = var.project_id
+#    account_id   = "${var.resource_prefix}-gsa-dataform"
+#    display_name = "Service Account for Dataform (executing Dataform Workflows)"
+#}
 
 # Set correct roles on Dataform Service Account
-resource "google_project_iam_member" "dataform" {
-    for_each = toset([
-        "roles/bigquery.dataEditor", 
-        "roles/bigquery.jobUser",
-        "roles/dataform.admin",
-        "roles/secretmanager.secretAccessor",
-        "roles/iam.serviceAccountUser"
-    ])
-    project = var.project_id
-    role    = each.value
-    member  = "serviceAccount:${google_service_account.dataform.email}"
-    depends_on = [
-        google_service_account.dataform
-    ]
-}
+#resource "google_project_iam_member" "dataform" {
+#    for_each = toset([
+#        "roles/bigquery.dataEditor", 
+#        "roles/bigquery.jobUser",
+#        "roles/dataform.admin",
+#        "roles/secretmanager.secretAccessor",
+#        "roles/iam.serviceAccountUser"
+#    ])
+#    project = var.project_id
+#    role    = each.value
+#    member  = "serviceAccount:${google_service_account.dataform.email}"
+#    depends_on = [
+#        google_service_account.dataform
+#    ]
+#}
 
 # Set correct roles on default Dataform Service Account
 resource "google_project_iam_member" "dataform" {
@@ -34,6 +34,6 @@ resource "google_project_iam_member" "dataform" {
     role    = each.value
     member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-dataform.iam.gserviceaccount.com"
     depends_on = [
-        google_service_account.dataform
+        google_dataform_repository.datahub
     ]
 }
